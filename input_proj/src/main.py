a8f118fc030f1b6f6201e9a026b5ea8ccdb7c4bb
Mikkle <Mikkelbb@gmail.com>
2018-04-14 10:37:37 +0200

    File struct johannes is gonna fix

---
+++ input_proj/src/main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import tensorflow as tf
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+"""makes the model for the cnn"""
+def model():    
+   pass 
+
+
+if __name__ == '__main__':
+    tf.app.run()
